32e7066d27b2864cd6692dadd8bd34ac3bfb67fc
Dan Andersson <dan.andersson@outlook.com>
2016-11-17 00:10:16 +0100

    Changed to command pattern

--- calculator.js
+++ calculator.js
@@ -1,72 +1,35 @@
 var calculator = (function () {
-    var input = '';
-    var expression = [];
+    var entry = '';
+    var status = '';
 
-    var calculate = function (firstNumber, operation) {
-        return function (secondNumber) {
-            return operation(parseInt(firstNumber, 10), parseInt(secondNumber, 10));
-        }
-    };
-
-    var operations = {
-        '+': function (a, b) {
-            return a + b;
+    var commands = {
+        '1': function () {
+            entry = '1';
+            status = '1';
+            return true;
         },
-        '-': function (a, b) {
-            return a - b;
-        },
-        '*': function (a, b) {
-            return a * b;
-        },
-        '/': function (a, b) {
-            return a / b;
-        }
-    };
-
-    var getResult = function () {
-        return expression.reduce(function (firstSymbol, secondSymbol) {
-            if (typeof firstSymbol === 'function') {
-                return firstSymbol(secondSymbol);
+        '+': function () {
+            if (entry !== '') {
+                return true;
             }
-
-            return calculate(firstSymbol, operations[secondSymbol]);
-        });
-    };
-
-    var isOperator = function (input) {
-        return operations.hasOwnProperty(input) || input === '=';
+            return false;
+        }
     };
 
     return {
-        getInput: function () {
-            return input || '0';
+        getEntry: function () {
+            return entry || '0';
         },
-
-        getOutput: function () {
-            return expression.join('') + input || '0';
+        getStatus: function () {
+            return status || '0';
         },
-
-        inputDigit: function (digit) {
-            if (isOperator(input)) {
-                expression.push(input);
-                input = '';
+        inputCommand: function (name) {
+            var command = commands[name];
+            if (!command) {
+                throw new Error('Invalid Command');
             }
-            input += digit.toString();
-        },
-
-        inputOperator: function (operator) {
-            if (isOperator(input) && !isOperator(operator)) { return; }
-
-            expression.push(input);
-            if (operator === '=') {
-                var result = getResult();
-                expression.push(operator);
-                input = result.toString();
-            } else {
-                input = operator;
-            }
-        },
-
+            return command();
+        }
     };
 })();
 
--- calculator.test.js
+++ calculator.test.js
@@ -5,75 +5,46 @@ describe('calculator', function() {
         calculator = require('./calculator');
     });
 
-    it('should have an input with the value "0"', function() {
-        expect(calculator.getInput()).toBe('0');
+    it('should display entry "0""', function () {
+        var entry = calculator.getEntry();
+        expect(entry).toBe('0');
     });
 
-    it('should have an output with the value "0"', function() {
-        expect(calculator.getOutput()).toBe('0');
+    it('should display status "0"', function () {
+        var status = calculator.getStatus();
+        expect(status).toBe('0');
     });
 
-    it('inputing digit 1 should return input "1"', function() {
-        calculator.inputDigit(1);
-        expect(calculator.getInput()).toBe('1');
+    it('inputting command "1" should return true', function () {
+        var inputSucceeded = calculator.inputCommand(1);
+        expect(inputSucceeded).toBe(true);
     });
 
-    it('inputing digit 1 should return output "1"', function() {
-        calculator.inputDigit(1);
-        expect(calculator.getOutput()).toBe('1');
+    it('inputting command "+" should return false', function () {
+        var inputSucceeded = calculator.inputCommand('+');
+        expect(inputSucceeded).toBe(false);
     });
 
-    it('inputing digits 1 and 2 should return input "12"', function() {
-        calculator.inputDigit(1);
-        calculator.inputDigit(2);
-        expect(calculator.getInput()).toBe('12');
+    it('inputting command "12" should throw error "Invalid Command"', function () {
+        var result = function () { return calculator.inputCommand(12) };
+        expect(result).toThrowError('Invalid Command');
     });
 
-    it('inputing digits 1 and 2 should return output "12"', function() {
-        calculator.inputDigit(1);
-        calculator.inputDigit(2);
-        expect(calculator.getOutput()).toBe('12');
+    it('inputting command "1" should display entry "1"', function () {
+        calculator.inputCommand(1);
+        var entry = calculator.getEntry();
+        expect(entry).toBe('1');
     });
 
-    it('inputing digit 1 and operator "+" should return input "+"', function() {
-        calculator.inputDigit(1);
-        calculator.inputOperator('+');
-        expect(calculator.getInput()).toBe('+');
+    it('inputting command "1" should display status "1"', function () {
+        calculator.inputCommand(1);
+        var status = calculator.getStatus();
+        expect(status).toBe('1');
     });
 
-    it('inputing digit 1 and operator "+" should return output "1+"', function() {
-        calculator.inputDigit(1);
-        calculator.inputOperator('+');
-        expect(calculator.getOutput()).toBe('1+');
-    });
-
-    it('inputing digit 1, operator "+" and digit 2 should return input "2"', function() {
-        calculator.inputDigit(1);
-        calculator.inputOperator('+');
-        calculator.inputDigit(2);
-        expect(calculator.getInput()).toBe('2');
-    });
-
-    it('inputing digit 1, operator "+" and digit 2 should return output "1+2"', function() {
-        calculator.inputDigit(1);
-        calculator.inputOperator('+');
-        calculator.inputDigit(2);
-        expect(calculator.getOutput()).toBe('1+2');
-    });
-
-    it('inputing digit 1, operator "+", digit 2 and operator "=" should return input "3"', function() {
-        calculator.inputDigit(1);
-        calculator.inputOperator('+');
-        calculator.inputDigit(2);
-        calculator.inputOperator('=');
-        expect(calculator.getInput()).toBe('3');
-    });
-
-    it('inputing digit 1, operator "+", digit 2 and operator "=" should return output "1+2=3"', function() {
-        calculator.inputDigit(1);
-        calculator.inputOperator('+');
-        calculator.inputDigit(2);
-        calculator.inputOperator('=');
-        expect(calculator.getOutput()).toBe('1+2=3');
+    it('inputting commands "1", "+" should return true', function () {
+        calculator.inputCommand(1);
+        var inputSucceeded = calculator.inputCommand('+');
+        expect(inputSucceeded).toBe(true);
     });
 });
